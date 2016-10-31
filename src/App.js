72c6d0f3272d3d800500fc5e62875af4e3dedc13
kingPuppy <michaeljlhuey@gmail.com>
2016-10-30 22:59:38 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
                         </div>
                     </div>
                     <div className="row name-header" onClick={this._onClick.bind(this)}>
-                        Daisy Buchanan {this.props.store.count}
+                        Jordan Baker {this.props.store.count}
                     </div>
                     <div className="row main">
                         <div className="col-md-4 main-img">
@@ -69,13 +69,13 @@ class App extends Component {
 
                             </div>
                             <div className="row about">
-                                <div><b>Age:</b> 22</div>
+                                <div><b>Age:</b> 29</div>
                                 <div><b>Gender:</b> Female</div>
-                                <div><b>City:</b> Melbourne</div>
-                                <div><b>Country:</b> Australia</div>
-                                <div><b>Interests:</b> Soccer, Jogging, Wine</div>
+                                <div><b>City:</b> ADK, NY</div>
+                                <div><b>Country:</b> United States</div>
+                                <div><b>Interests:</b> Golf, Jogging, Wine</div>
                                 <div><b>Pets:</b> My dog Bruno</div>
-                                <div><b>About me:</b> I am a super athletic girl who likes to have fun, and am looking for a guy who is athletic and likes to be outdoors.</div>
+                                <div><b>About me:</b> I am a super athletic girl who likes to have fun, a little cynical and am looking for a guy who is fun and likes to be outdoors.</div>
                             </div>
                         </div>
                     </div>
